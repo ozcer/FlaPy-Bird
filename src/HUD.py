971dadb7d74b763dc6182b9deda5d25891d1959f
ozcer <iamreallyoscar@hotmail.com>
2018-03-07 21:05:39 -0800

    togglable fps display

--- src/HUD.py
+++ src/HUD.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 
 from src.const import *
 
@@ -9,9 +10,20 @@ class HUD():
         self.game = game
         
         self.fonts = {"fps": pygame.font.SysFont("monospace", 20)}
+        
+        self.dev_mode = False
+
+    def handle_input(self):
+        keys = pygame.key.get_pressed()
+        
+        # developer mode
+        if (keys[K_LCTRL] or keys[K_RCTRL]) and keys[K_m]:
+            self.dev_mode = not self.dev_mode
     
     def render(self):
-        fps_display = self.fonts["fps"].render(f"{round(self.game.fps_clock.get_fps())}",
-                                               True,
-                                               BLACK)
-        self.game.surface.blit(fps_display, (DISPLAY_WIDTH-fps_display.get_width(),0))
+        self.handle_input()
+        if self.dev_mode:
+            fps_display = self.fonts["fps"].render(f"{round(self.game.fps_clock.get_fps())}",
+                                                   True,
+                                                   BLACK)
+            self.game.surface.blit(fps_display, (DISPLAY_WIDTH-fps_display.get_width(),0))
